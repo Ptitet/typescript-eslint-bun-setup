b42f981b0de774b29df50c2b25def05d49544013
Le Michel <95184938+Ptitet@users.noreply.github.com>
2024-06-28 11:40:16 +0200

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        languageOptions: {
+            parserOptions: {
+                project: true
+            }
+        },
+        // ESLint
+        rules: {
+            'no-constant-condition': ['error', {
+                checkLoops: 'allExceptWhileTrue'
+            }]
+        }
+    },
+    // TypeScript
+    {
+        rules: {
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-unnecessary-condition': ['error', {
+                allowConstantLoopConditions: true
+            }],
+            '@typescript-eslint/restrict-template-expressions': ['error', {
+                allowNumber: true
+            }]
+        }
+    },
+    // Stylistic
+    {
+        rules: {
+            '@stylistic/member-delimiter-style': ['error', {
+                multiline: {
+                    delimiter: 'semi'
+                }
+            }],
+            '@stylistic/eol-last': 'error',
+            '@stylistic/semi': 'error',
+            '@stylistic/quotes': ['error', 'single']
+        }
+    }
+);
